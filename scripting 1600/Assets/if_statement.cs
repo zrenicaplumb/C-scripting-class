6a148fa508f91d9f0d91f7c3fdf177eafcf0b1d6
unknown <10561898@CS514-17.ad.uvu.edu>
2015-10-06 08:56:41 -0600

    adding project folder to correct repo. folder

---
+++ scripting 1600/Assets/if_statement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class if_statement : MonoBehaviour {
+    public float velocity = 85.0f;
+    public float maxVelocity = 70.0f;
+    public float minVelocity = 40.0f;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            velocityTest();
+
+        velocity -= Time.deltaTime * 5;
+
+        
+    }
+
+    void velocityTest() {
+        if(velocity > maxVelocity)
+        {
+            print("too fast");
+        }
+       else if(velocity < minVelocity)
+        {
+            print("too slow.");
+        }
+        else
+        {
+            print("good speed");
+        }
+    }
+}
