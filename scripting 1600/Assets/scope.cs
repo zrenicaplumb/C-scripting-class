bc6bde35aa9ee45dcdc509590024bbe4464a23d4
unknown <10561898@CS514-17.ad.uvu.edu>
2015-10-08 10:06:32 -0600

    no message

---
+++ scripting 1600/Assets/scope.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scope : MonoBehaviour
+{
+    public int bob = 5;
+    // Use this for initialization
+    void Start() {
+        public class rick {
+        public int levelOfDrunkenness;
+        public drinkWhiskey(int alcoholContent)
+        {
+            if (alcoholContent > 5)
+            {
+                print("this is good stuff morty!");
+                levelOfDrunkeness = 5;
+                print("rick is now" + levelofDrunkenness + "% drunk!");
+            }
+
+               
+            
+            else if (alcoholContent > 50)
+            {
+                print("morty this stuff is too strong im gonna die!");
+                levelOfDrunkenness = 100;
+                print("rick is now" + levelofDrunkenness + "% drunk!");
+            }
+
+               
+        }
+        
+      
+    }
+
+      
+
+
+
+    rick.drinkWhiskey(5); 
+    }
+
+
+    
+	
+}
+
+
+
